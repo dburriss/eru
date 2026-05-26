dac45b7a188c79b0e6752896d5f34e04609c3e79
Devon Burriss <devon.b@outlook.com>
2026-05-26 14:23:48 +0200

    Refactor command parsing in `Program.fs` to improve structure and clarity; streamline command handling for Init, Add, Search, Sync, and SourceAdd commands

--- src/Eru.Cli/Program.fs
+++ src/Eru.Cli/Program.fs
@@ -6,28 +6,49 @@ open Eru.Adapters
 
 let (|InitCmd|_|) (r: ParseResults<EruArgs>) =
     r.TryGetSubCommand() |> Option.bind (function
-        | EruArgs.Init args -> Some args
+        | EruArgs.Init args -> Some { Init.Command.Force = args.Contains InitArgs.Force }
         | _                 -> None)
 
 let (|AddCmd|_|) (r: ParseResults<EruArgs>) =
     r.TryGetSubCommand() |> Option.bind (function
-        | EruArgs.Add args -> Some args
-        | _                -> None)
+        | EruArgs.Add args ->
+            let cmd : Add.Command = {
+                RemotePath     = args.TryGetResult AddArgs.Remote_Path
+                Tags           = args.GetResults  AddArgs.Tag
+                SourceName     = args.TryGetResult AddArgs.Source
+                CollectionName = args.TryGetResult AddArgs.Collection
+                Target         = args.TryGetResult AddArgs.Target
+            }
+            Some cmd
+        | _ -> None)
 
 let (|SearchCmd|_|) (r: ParseResults<EruArgs>) =
     r.TryGetSubCommand() |> Option.bind (function
-        | EruArgs.Search args -> Some args
-        | _                   -> None)
+        | EruArgs.Search args ->
+            let query : Search.Query = {
+                Terms = args.GetResult(SearchArgs.Terms, defaultValue = [])
+                Tags  = args.GetResults SearchArgs.Tag
+            }
+            Some query
+        | _ -> None)
 
 let (|SyncCmd|_|) (r: ParseResults<EruArgs>) =
     r.TryGetSubCommand() |> Option.bind (function
-        | EruArgs.Sync args -> Some args
+        | EruArgs.Sync args -> Some { Sync.Options.DryRun = args.Contains SyncArgs.Dry_Run }
         | _                 -> None)
 
-let (|SourceCmd|_|) (r: ParseResults<EruArgs>) =
+let (|SourceAddCmd|_|) (r: ParseResults<EruArgs>) =
     r.TryGetSubCommand() |> Option.bind (function
-        | EruArgs.Source args -> Some args
-        | _                   -> None)
+        | EruArgs.Source args ->
+            args.TryGetSubCommand() |> Option.map (fun (SourceArgs.Add addArgs) ->
+                {
+                    Url      = addArgs.GetResult SourceAddArgs.Url
+                    Name     = addArgs.TryGetResult SourceAddArgs.Name
+                    Branch   = addArgs.TryGetResult SourceAddArgs.Branch
+                    BasePath = addArgs.TryGetResult SourceAddArgs.Basepath
+                    IsGlobal = addArgs.Contains SourceAddArgs.Global
+                } : Source.AddCommand)
+        | _ -> None)
 
 [<EntryPoint>]
 let main argv =
@@ -37,46 +58,11 @@ let main argv =
         let deps   = AdapterDeps.create ()
 
         match parsed with
-        | InitCmd args ->
-            let cmd : Init.Command = { Force = args.Contains InitArgs.Force }
-            Init.run deps cmd
-
-        | AddCmd args ->
-            let cmd : Add.Command = {
-                RemotePath     = args.TryGetResult AddArgs.Remote_Path
-                Tags           = args.GetResults  AddArgs.Tag
-                SourceName     = args.TryGetResult AddArgs.Source
-                CollectionName = args.TryGetResult AddArgs.Collection
-                Target         = args.TryGetResult AddArgs.Target
-            }
-            Add.run deps cmd
-
-        | SearchCmd args ->
-            let query : Search.Query = {
-                Terms = args.GetResult(SearchArgs.Terms, defaultValue = [])
-                Tags  = args.GetResults SearchArgs.Tag
-            }
-            Search.run deps query
-
-        | SyncCmd args ->
-            let opts : Sync.Options = { DryRun = args.Contains SyncArgs.Dry_Run }
-            Sync.run deps opts
-
-        | SourceCmd args ->
-            match args.TryGetSubCommand() with
-            | Some (SourceArgs.Add addArgs) ->
-                let cmd : Source.AddCommand = {
-                    Url      = addArgs.GetResult SourceAddArgs.Url
-                    Name     = addArgs.TryGetResult SourceAddArgs.Name
-                    Branch   = addArgs.TryGetResult SourceAddArgs.Branch
-                    BasePath = addArgs.TryGetResult SourceAddArgs.Basepath
-                    IsGlobal = addArgs.Contains SourceAddArgs.Global
-                }
-                Source.add deps cmd
-            | _ ->
-                printfn "%s" (parser.PrintUsage())
-                0
-
+        | InitCmd cmd      -> Init.run   deps cmd
+        | AddCmd cmd       -> Add.run    deps cmd
+        | SearchCmd query  -> Search.run deps query
+        | SyncCmd opts     -> Sync.run   deps opts
+        | SourceAddCmd cmd -> Source.add deps cmd
         | _ ->
             printfn "%s" (parser.PrintUsage())
             0
